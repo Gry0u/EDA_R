bba8a65fb69747027423811ac763ca7aa9a641cb
gauthier <gauthriou@protonmail.com>
2018-05-29 22:48:00 +0200

    stack

--- gapminder.R
+++ gapminder.R
@@ -4,7 +4,10 @@ library(gridExtra)
 library(xlsx)
 
 energy <- read.xlsx('energy use total.xlsx', sheetIndex = 1 )
-str(energy)
+energy <- stack(energy)
 internet_users <- read.xlsx('Internet user total.xlsx', sheetIndex = 1)
+internet_users <- stack(internet_users)
 
-ggplot(aes(x))
+energy$ind <- charenergy$ind
+table(energy$ind)
+sapply(energy, class)
